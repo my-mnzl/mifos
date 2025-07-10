21399d81cc4494b3452ab192b2c2664625ef5d3d
Austin Matsick <ajmatsick@gmail.com>
2025-07-10 08:00:56 +0200

    Show local storage date value in footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -137,4 +137,17 @@ export class FooterComponent implements OnInit, OnDestroy {
       this.isBusinessDateDefined = true;
     });
   }
+
+  get cachedBusinessDate(): Date {
+    return this.settingsService.businessDate;
+  }
+
+  setCachedBusinessDate(): void {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const date = `${now.getDate()}`.padStart(2, '0');
+    const dateString = `${year}-${month}-${date}`;
+    this.settingsService.setBusinessDate(dateString);
+  }
 }
